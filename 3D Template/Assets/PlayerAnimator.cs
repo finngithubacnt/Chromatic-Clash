12c24d7e39519272d1ee72c196f755e257a8e5dd
fgreenmun@gmail.com <fgreenmun@gmail.com>
2025-09-21 16:53:11 -0700

    this might break things because of file sizes, might have to pike myself( player animaitoins and charecter)

---
+++ 3D Template/Assets/PlayerAnimator.cs
@@ -0,0 +1,36 @@
+using Unity.Netcode;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]// do reaserch , makes no sense why it only works with this
+public class PlayerAnimator : NetworkBehaviour
+{
+    private Animator animator;
+    private CharacterController controller;
+
+    private int speedHash = Animator.StringToHash("Speed");
+    private int jumpHash = Animator.StringToHash("IsJumping");
+    private int crouchHash = Animator.StringToHash("IsCrouching");
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (!IsOwner) return; 
+
+        // fnd movement speed
+        Vector3 horizontalVel = new Vector3(controller.velocity.x, 0, controller.velocity.z);
+        float speed = horizontalVel.magnitude;
+
+        // set parameters locally(my player not yours)
+        animator.SetFloat(speedHash, speed);
+
+        animator.SetBool(jumpHash, !controller.isGrounded);
+
+        bool isCrouching = Input.GetKey(KeyCode.C); 
+        animator.SetBool(crouchHash, isCrouching);
+    }
+}
